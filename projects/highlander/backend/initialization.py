66ffeb0af40da7236ca83c05b8edf1815bd24b1b
Mattia <m.dantonio81@gmail.com>
2021-03-16 15:20:13 +0100

    Minor changes

--- projects/highlander/backend/initialization.py
+++ projects/highlander/backend/initialization.py
@@ -1,7 +1,8 @@
-from restapi.connectors import sqlalchemy
+# from restapi.connectors import sqlalchemy
 
 
 class Initializer:
+
     """
     This class is instantiated just after restapi init
     Implement the constructor to add operations performed one-time at initialization
