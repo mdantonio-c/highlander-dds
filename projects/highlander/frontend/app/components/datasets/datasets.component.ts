e1f089570d4f387e9bea6744a56ae792b14a494c
Giuseppe Trotta <g.trotta@cineca.it>
2021-04-22 12:13:20 +0200

    minor fix

--- projects/highlander/frontend/app/components/datasets/datasets.component.ts
+++ projects/highlander/frontend/app/components/datasets/datasets.component.ts
@@ -11,9 +11,7 @@ import { Observable } from "rxjs";
   styleUrls: ["./datasets.component.css"],
 })
 export class DatasetsComponent implements OnInit {
-  readonly title = "Datasets";
-  loading: boolean = false;
-  datasets: Dataset[] = [];
+  datasets: Dataset[];
 
   constructor(
     private dataService: DataService,
@@ -26,21 +24,19 @@ export class DatasetsComponent implements OnInit {
   }
 
   private loadDatasets() {
-    this.loading = true;
     this.spinner.show();
     this.dataService
       .getDatasets()
       .subscribe(
         (data) => {
           this.datasets = data;
-          console.log(this.datasets);
+          // console.log(this.datasets);
         },
         (error) => {
           this.notify.showError(error);
         }
       )
       .add(() => {
-        this.loading = false;
         this.spinner.hide();
       });
   }
