6bd8af97c02ddad73967c222d79d599bfc540311
Giuseppe Trotta <g.trotta@cineca.it>
2021-05-05 10:12:39 +0200

    fix catalog cache path

--- projects/highlander/backend/endpoints/datasets.py
+++ projects/highlander/backend/endpoints/datasets.py
@@ -11,7 +11,7 @@ from restapi.utilities.logs import log
 CATALOG_DIR = os.environ.get("CATALOG_DIR", "/catalog")
 broker = DataBroker(
     catalog_path=f"{CATALOG_DIR}/catalog.yaml",  # Place where catalog YAML file is located
-    cache_dir=f"{CATALOG_DIR}/catalog/cache",  # Directory where cache files should be stored
+    cache_dir=f"{CATALOG_DIR}/cache",  # Directory where cache files should be stored
     cache_details=True,  # If details should be cached as well
     storage=f"{CATALOG_DIR}/download",  # Directory where retrieved data are persisted
     log_path=f"{CATALOG_DIR}/logs",  # Directory where logs should be saved
