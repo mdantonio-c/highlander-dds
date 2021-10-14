94b18395ea142758b32997d8004faee4b70d4881
Giuseppe Trotta <g.trotta@cineca.it>
2021-10-14 18:12:16 +0200

    add test for usage endpoint; fix typing issues

--- projects/highlander/backend/tests/endpoints/test_datasets.py
+++ projects/highlander/backend/tests/endpoints/test_datasets.py
@@ -1,4 +1,5 @@
-from restapi.rest.definition import Response
+from typing import Any, List
+
 from restapi.tests import API_URI, BaseTests, FlaskClient
 
 __author__ = "Giuseppe Trotta (g.trotta@cineca.it)"
@@ -6,14 +7,10 @@ DATASET_VHR = "era5-downscaled-over-italy"
 PRODUCT_VHR = "VHR-REA_IT_1989_2020_hourly"
 
 
-def get_response(client: FlaskClient, endpoint) -> Response:
-    return client.get(endpoint)
-
-
 class TestApp(BaseTests):
     def test_get_datasets(self, client: FlaskClient) -> None:
         """Expected no empty dataset catalog"""
-        r = get_response(client, f"{API_URI}/datasets")
+        r = client.get(f"{API_URI}/datasets")
         assert r.status_code == 200
 
         # check response type
@@ -22,7 +19,7 @@ class TestApp(BaseTests):
         assert len(response_data) > 0
 
     def test_get_applications(self, client: FlaskClient) -> None:
-        r = get_response(client, f"{API_URI}/datasets?application=true")
+        r = client.get(f"{API_URI}/datasets?application=true")
         assert r.status_code == 200
 
         # check response type
@@ -34,20 +31,19 @@ class TestApp(BaseTests):
 
     def test_get_dataset(self, client: FlaskClient) -> None:
         # expected era5-downscaled-over-italy dataset in test data
-        r = get_response(client, f"{API_URI}/datasets/{DATASET_VHR}")
+        r = client.get(f"{API_URI}/datasets/{DATASET_VHR}")
         assert r.status_code == 200
 
     def test_get_dataset_product(self, client: FlaskClient) -> None:
         # expected VHR-REA_IT_1989_2020_hourly product in test data
-        r = get_response(
-            client, f"{API_URI}/datasets/{DATASET_VHR}/products/{PRODUCT_VHR}"
-        )
+        r = client.get(f"{API_URI}/datasets/{DATASET_VHR}/products/{PRODUCT_VHR}")
         assert r.status_code == 200
 
     def test_get_dataset_image(self, client: FlaskClient) -> None:
-        r = get_response(client, f"{API_URI}/datasets")
+        r = client.get(f"{API_URI}/datasets")
         datasets = self.get_content(r)
+        assert isinstance(datasets, list)
         # iterates over each dataset
         for ds in datasets:
-            get_response(client, f"{API_URI}/datasets/{ds['id']}/image")
+            client.get(f"{API_URI}/datasets/{ds['id']}/image")
             assert r.status_code in [200, 404]
