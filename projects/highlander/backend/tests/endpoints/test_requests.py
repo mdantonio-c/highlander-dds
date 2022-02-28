a027e29d140ef1d61cdaa7072392c33914de88f9
Giuseppe Trotta <g.trotta@cineca.it>
2022-02-28 17:18:28 +0100

    add backend tests for data extraction

--- projects/highlander/backend/tests/endpoints/test_requests.py
+++ projects/highlander/backend/tests/endpoints/test_requests.py
@@ -1,18 +1,33 @@
-from typing import Any
+import json
+from typing import Any, Dict
 
-from restapi.rest.definition import Response
+import pytest
+from restapi.connectors import celery, sqlalchemy
 from restapi.tests import API_URI, BaseTests, FlaskClient
 
 __author__ = "Giuseppe Trotta (g.trotta@cineca.it)"
+DATASET_NAME = "era5-downscaled-over-italy"
+PRODUCT_NAME = "VHR-REA_IT_1989_2020_hourly"
+PRODUCT_FORMAT = "netcdf"
 
 
 class TestApp(BaseTests):
     def test_get_user_requests(self, client: FlaskClient) -> None:
+        endpoint = f"{API_URI}/requests"
+
         # test without login
-        r = client.get(f"{API_URI}/requests")
+        r = client.get(endpoint)
         assert r.status_code == 401
 
-        # TODO
+        # test authenticated user : default
+        headers, _ = self.do_login(client, None, None)
+        r = client.get(endpoint, headers=headers)
+        assert r.status_code == 200
+
+        # test response content
+        assert r.headers["Content-Type"] == "application/json"
+        response_body = self.get_content(r)
+        assert isinstance(response_body, list)
 
     def test_get_a_request(self, client: FlaskClient) -> None:
         # TODO
@@ -22,10 +37,65 @@ class TestApp(BaseTests):
         # TODO
         pass
 
-    def test_estimate_size(self, client: FlaskClient) -> None:
-        # TODO
-        pass
+    def test_estimate_size(
+        self, client: FlaskClient, data_filter: Dict[str, Any], headers
+    ) -> None:
+        endpoint = f"{API_URI}/estimate-size"
 
-    def test_submit_data_extraction(self, client: FlaskClient) -> None:
-        # TODO
-        pass
+        # submit size estimation request
+        r = client.post(f"{endpoint}/{DATASET_NAME}", data=data_filter, headers=headers)
+        assert r.status_code == 200
+
+        response_body = self.get_content(r)
+        assert isinstance(response_body, int)
+
+    def test_data_extraction(self, client: FlaskClient, data_request, headers) -> None:
+
+        # check task execution results
+        c = celery.get_instance()
+        task_id = data_request.task_id
+        task = c.celery_app.AsyncResult(task_id)
+        assert task is not None
+        result = task.get(timeout=5)  # FIXME to manage
+        assert result is None
+
+        db = sqlalchemy.get_instance()  # can we do better?
+        db_request = db.Request.query.filter_by(task_id=task_id).first()
+        assert db_request.status == "SUCCESS"
+
+    @pytest.fixture
+    def headers(self, client: FlaskClient):
+        """login: default user"""
+        headers, _ = self.do_login(client, None, None)
+        return headers
+
+    @pytest.fixture
+    def data_request(self, data_filter, client, headers):
+        """submit data request"""
+        endpoint = f"{API_URI}/requests"
+        r = client.post(f"{endpoint}/{DATASET_NAME}", data=data_filter, headers=headers)
+        assert r.status_code == 202
+
+        task_id = self.get_content(r)
+        assert isinstance(task_id, str)
+
+        # check a task request has been created
+        db = sqlalchemy.get_instance()
+        db_request = db.Request.query.filter_by(task_id=task_id).first()
+        assert db_request is not None
+        assert db_request.status == "PENDING"
+        yield db_request
+
+        # teardown test request
+        r = client.delete(f"{API_URI}/requests/{db_request.id}", headers=headers)
+        assert r.status_code == 200
+
+    @pytest.fixture
+    def data_filter(self) -> Dict[str, Any]:
+        data: Dict[str, Any] = {
+            "product": PRODUCT_NAME,
+            "format": PRODUCT_FORMAT,
+            "variable": json.dumps(["air_temperature"]),
+        }
+        # add any other filters
+        return data
