98391547de036dcb4668349849325d0717d1ec40
Giuseppe Trotta <g.trotta@cineca.it>
2022-08-08 15:41:18 +0200

    replace geoserver-rest module with custom Geoserver class for create_shp_datastore

--- projects/highlander/backend/tasks/geoserver.py
+++ projects/highlander/backend/tasks/geoserver.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from pathlib import Path
+from typing import Optional
 from zipfile import ZipFile
 
+import requests
 from celery.app.task import Task
 from highlander.constants import DATASETS_DIR
 from highlander.tasks.crop_water import CROP_WATER_AREAS
 from restapi.connectors.celery import CeleryExt
+from restapi.env import Env
 from restapi.utilities.logs import log
 
 
@@ -22,6 +25,9 @@ def generate_layers(self: Task, ref_date: str):
         date_obj = datetime.strptime(ref_date, "%Y-%m-%d")
     except ValueError:
         raise ValueError("Incorrect date format, should be YYYY-MM-DD")
+
+    geo = Geoserver(service_url="http://geoserver.dockerized.io:8080/geoserver")
+
     for area in CROP_WATER_AREAS:
         target_path = Path(
             DATASETS_DIR.joinpath("crop-water"),
@@ -40,4 +46,59 @@ def generate_layers(self: Task, ref_date: str):
             with ZipFile(zip_file, "w") as archive:
                 for file_path in list_files:
                     archive.write(file_path, arcname=file_path.name)
-        # TODO use geoserver rest API
+        # use geoserver rest API to create a shapefile store
+        shp_store = f"{area}_monthlyForecast_{ref_date}"
+        log.info(f"Creating SHP datastore <{shp_store}>")
+        res = geo.create_shp_datastore(
+            path=zip_file, store_name=shp_store, workspace="highlander"
+        )
+        log.info(res)
+
+    log.info(f"Task <{self.name}, args='{ref_date}'> completed")
+
+
+class Geoserver:
+    def __init__(
+        self,
+        service_url: str = "http://localhost:8080/geoserver",
+        username: str = Env.get("GEOSERVER_ADMIN_USER", "admin"),
+        password: str = Env.get("GEOSERVER_ADMIN_PASSWORD", "geoserver"),
+    ):
+        self.service_url = service_url
+        self.username = username
+        self.password = password
+
+    def create_shp_datastore(
+        self,
+        path: Path,
+        store_name: Optional[str] = None,
+        workspace: Optional[str] = None,
+        file_extension: str = "shp",
+    ):
+        if path is None:
+            raise Exception("You must provide a full path to shapefile")
+
+        if workspace is None:
+            workspace = "default"
+
+        if store_name is None:
+            store_name = path.stem
+
+        headers = {"Content-type": "application/zip"}
+        url = "{0}/rest/workspaces/{1}/datastores/{2}/file.{3}?filename={2}&update=overwrite".format(
+            self.service_url, workspace, store_name, file_extension
+        )
+        with open(path, "rb") as f:
+            r = requests.put(
+                url,
+                data=f.read(),
+                auth=(self.username, self.password),
+                headers=headers,
+            )
+
+            if r.status_code in [200, 201]:
+                return "The shapefile datastore created successfully!"
+            else:
+                raise Exception(
+                    f"{r.status_code}: The shapefile datastore can not be created! {r.content}"
+                )
