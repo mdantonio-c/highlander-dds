62f3b797d1580a3e294b05ebb373595bcbd7c1e5
Giuseppe Trotta <g.trotta@cineca.it>
2022-10-04 15:14:21 +0200

    upgrade to rapydo 2.4

--- projects/highlander/frontend/app/components/data-extraction-modal/data-extraction-modal.component.ts
+++ projects/highlander/frontend/app/components/data-extraction-modal/data-extraction-modal.component.ts
@@ -277,12 +277,12 @@ export class DataExtractionModalComponent implements OnInit, OnDestroy {
     data.widgets_order.forEach((w) => {
       let comp = this.getWidget(w);
       if (comp.type === "StringList") {
-        formGroup.addControl(comp.name, new FormArray([]));
+        formGroup.addControl(comp.name as any, new FormArray([]));
       } else if (
         comp.type === "ExclusiveFrame" &&
         !["temporal_coverage", "spatial_coverage"].includes(comp.name)
       ) {
-        formGroup.addControl(comp.name, new FormArray([]));
+        formGroup.addControl(comp.name as any, new FormArray([]));
       }
     });
     return formGroup;
