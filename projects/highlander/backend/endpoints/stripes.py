7ee3f51642a126de46695da7a416a21020043961
bchiavarini <b.chiavarini@cineca.it>
2022-10-21 16:03:17 +0200

    #49 map crop api adapted for forest suitability

--- projects/highlander/backend/endpoints/stripes.py
+++ projects/highlander/backend/endpoints/stripes.py
@@ -1,30 +1,27 @@
-from pathlib import Path
-from typing import List, Optional, Any, Dict, Union
-
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-import numpy as np
 import math
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Union
 
-from flask import send_from_directory, send_file
 import geopandas as gpd
-import xarray as xr
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 import regionmask
-
+import xarray as xr
+from flask import send_file, send_from_directory
 from highlander.connectors import broker
-from restapi.connectors import Connector
-
-from restapi import decorators
-from restapi.exceptions import NotFound, ServerError
-from restapi.rest.definition import EndpointResource, Response
-from restapi.utilities.logs import log
-from restapi.models import Schema, fields, validate
 from marshmallow import ValidationError, pre_load
+from restapi import decorators
+from restapi.connectors import Connector
+from restapi.exceptions import NotFound, ServerError
+from restapi.models import Schema, fields, validate
+from restapi.rest.definition import EndpointResource, Response
+from restapi.services.authentication import User
+from restapi.utilities.logs import log
 
 STRIPES_OUTPUT_ROOT = Path("/catalog/climate_stripes/")
 
-#STRIPES_INPUT_ROOT = Path("/catalog/datasets/datasets/climate_stripes/")
+# STRIPES_INPUT_ROOT = Path("/catalog/datasets/datasets/climate_stripes/")
 
 ADMINISTRATIVES = ["Italy", "regions", "provinces"]
 
@@ -34,20 +31,21 @@ GEOJSON_PATH = "/catalog/assets"
 
 DATA_VARIABLE = "T_2M"
 
+
 def cropArea(
     netcdf_path: Path,
     area_name: str,
     area: Any,
     data_variable: str = DATA_VARIABLE,
-    ) -> Any:
+) -> Any:
     # Function to crop the data to be plotted.
     # Read the netcdf file.
     data_to_crop = xr.open_dataset(netcdf_path)
 
     # Create the polygon mask.
     polygon_mask = regionmask.Regions(
-         name=area_name,
-         outlines=list(area.geometry.values[i] for i in range(0, area.shape[0])),
+        name=area_name,
+        outlines=list(area.geometry.values[i] for i in range(0, area.shape[0])),
     )
     mask = polygon_mask.mask(data_to_crop, lat_name="lat", lon_name="lon")
 
@@ -63,17 +61,17 @@ def plotStripes(array, yearsList: list, region_id: str, fileOutput: str):
     region_id = f"{region_id.replace('_', ' ').title()}"
     fig, ax = plt.subplots(figsize=(20, 8))
     fig.subplots_adjust(bottom=0.25, left=0.25)  # make room for labels
-    mpl.rcParams['font.size'] = 20
+    mpl.rcParams["font.size"] = 20
     min_val = math.floor(array.min())  # np.round(array.min()*10)/10
     max_val = math.ceil(array.max())  # np.round(array.max()*10)/10
     stripes = plt.pcolormesh(array, vmin=min_val, vmax=max_val, cmap="bwr")
-    cbar = plt.colorbar(stripes, label='Air temperature [°C]')
+    cbar = plt.colorbar(stripes, label="Air temperature [°C]")
     ax.set_title(region_id, alpha=1)
     ax.set_xticks(np.arange(array.shape[1]) + 0.5, minor=False)
     ax.set_xticklabels(yearsList, rotation=90, size=15)
     ax.axes.get_yaxis().set_visible(False)
     plt.show()
-    fig.savefig(fileOutput, transparent=True, bbox_inches='tight', pad_inches=0)
+    fig.savefig(fileOutput, transparent=True, bbox_inches="tight", pad_inches=0)
 
 
 class StripesDetails(Schema):
@@ -85,7 +83,8 @@ class StripesDetails(Schema):
     # Validation. Check whether an area_id is given when administrative is  "regions" or "provinces".
     @pre_load
     def params_validation(
-       self, data: Dict[str, Union[str, None]], **kwargs: Any) -> Dict[str, Union[str, None]]:
+        self, data: Dict[str, Union[str, None]], **kwargs: Any
+    ) -> Dict[str, Union[str, None]]:
         administrative = data.get("administrative")
         area_id = data.get("area_id", None)
         # check if area_id is needed
@@ -108,13 +107,16 @@ class Stripes(EndpointResource):
             500: "Errors in cropping or plotting the data",
         },
     )
-    @decorators.use_kwargs(
-        StripesDetails,
-        location="query"
-    )
+    @decorators.use_kwargs(StripesDetails, location="query")
     @decorators.auth.require()
-    def get(self, user, dataset_id: str, time_period: str, administrative: str,
-            area_id: Optional[str] = None) -> Response:
+    def get(
+        self,
+        user: User,
+        dataset_id: str,
+        time_period: str,
+        administrative: str,
+        area_id: Optional[str] = None,
+    ) -> Response:
 
         # Normalise area_id names to standard format that cope with the different geojson structures.
         if administrative == "regions" or administrative == "provinces":
@@ -142,7 +144,9 @@ class Stripes(EndpointResource):
         # Create an output file name and path:
         output_filename = f"{area_name.replace(' ', '_').lower()}_stripes.png"
         output_path = f"{time_period}/{administrative}"
-        output_dir = Path(STRIPES_OUTPUT_ROOT, output_path)  # Needed to create the output folder if it does not exist.
+        output_dir = Path(
+            STRIPES_OUTPUT_ROOT, output_path
+        )  # Needed to create the output folder if it does not exist.
         output_filepath = Path(output_dir, output_filename)
 
         # Check if the stripes have already been created.
@@ -159,35 +163,48 @@ class Stripes(EndpointResource):
             # We define a STRIPES_INPUT_ROOT using the details of a dataset available in the dds.
             dds = broker.get_instance()
             try:
-                product_urlpath = dds.broker.catalog[dataset_id]["VHR-REA_IT_1989_2020_hourly"].urlpath
-                product_urlpath_root = product_urlpath.split('vhr-rea')[0]
+                product_urlpath = dds.broker.catalog[dataset_id][
+                    "VHR-REA_IT_1989_2020_hourly"
+                ].urlpath
+                product_urlpath_root = product_urlpath.split("vhr-rea")[0]
             except Exception as exc:
                 raise NotFound(f"Unable to get dataset url root: {exc}")
 
             # Check if input data exists.
             input_filename = f"T_2M_1989-2020_{time_period}_anomalies.nc"
-            data_filepath = Path(product_urlpath_root, "climate_stripes", input_filename)
+            data_filepath = Path(
+                product_urlpath_root, "climate_stripes", input_filename
+            )
 
             if data_filepath.is_file() is False:
-                raise NotFound(f"Data file {input_filename} not found in {data_filepath}")
+                raise NotFound(
+                    f"Data file {input_filename} not found in {data_filepath}"
+                )
 
             # If necessary crop the area.
             if administrative == "regions" or administrative == "provinces":
                 try:
-                    nc_data_to_plot = cropArea(
-                        data_filepath, area_name, area
-                    )
+                    nc_data_to_plot = cropArea(data_filepath, area_name, area)
                 except Exception as exc:
                     raise ServerError(f"Errors in cropping the data: {exc}")
                 else:
-                    nc_data_to_plot_mean = nc_data_to_plot.mean(axis=(1, 2)).values.reshape((1, len(nc_data_to_plot.time)))
-                    nc_data_to_plot_years = [str(x.astype('datetime64[Y]')) for x in nc_data_to_plot.time.values]
+                    nc_data_to_plot_mean = nc_data_to_plot.mean(
+                        axis=(1, 2)
+                    ).values.reshape((1, len(nc_data_to_plot.time)))
+                    nc_data_to_plot_years = [
+                        str(x.astype("datetime64[Y]"))
+                        for x in nc_data_to_plot.time.values
+                    ]
             # Otherwise simply load data
             elif administrative == "Italy":
                 nc_data = xr.open_dataset(data_filepath)
                 nc_data_to_plot = nc_data[DATA_VARIABLE][:]
-                nc_data_to_plot_mean = nc_data_to_plot.mean(axis=(1, 2)).values.reshape((1, len(nc_data_to_plot.time)))
-                nc_data_to_plot_years = [str(x.astype('datetime64[Y]')) for x in nc_data_to_plot.time.values]
+                nc_data_to_plot_mean = nc_data_to_plot.mean(axis=(1, 2)).values.reshape(
+                    (1, len(nc_data_to_plot.time))
+                )
+                nc_data_to_plot_years = [
+                    str(x.astype("datetime64[Y]")) for x in nc_data_to_plot.time.values
+                ]
 
             # Create the output directory if it does not exists.
             if not output_dir.is_dir():
@@ -195,7 +212,12 @@ class Stripes(EndpointResource):
 
             # Plot stripes.
             try:
-                plotStripes(nc_data_to_plot_mean, nc_data_to_plot_years, area_name, output_filepath)
+                plotStripes(
+                    nc_data_to_plot_mean,
+                    nc_data_to_plot_years,
+                    area_name,
+                    output_filepath,
+                )
             except Exception as exc:
                 raise ServerError(f"Errors in plotting the data: {exc}")
 
@@ -204,4 +226,3 @@ class Stripes(EndpointResource):
             # Testing output:
             # res = f"OK / Stripes created at {output_filepath}"
             # return self.response(res)
-
