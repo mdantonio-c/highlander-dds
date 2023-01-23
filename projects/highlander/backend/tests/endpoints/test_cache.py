87850e6de47ba1808af8eaf9d6179d0af495e812
bchiavarini <b.chiavarini@cineca.it>
2023-01-23 13:20:07 +0100

    precommit on test cache file

--- projects/highlander/backend/tests/endpoints/test_cache.py
+++ projects/highlander/backend/tests/endpoints/test_cache.py
@@ -1,18 +1,18 @@
-import pytest
 import time
-from faker import Faker
 from pathlib import Path
-from flask import Flask
 
+import pytest
 from celery.result import AsyncResult
-
+from faker import Faker
+from flask import Flask
 from highlander.constants import CACHE_DIR
 from restapi.connectors.celery import Ignore
 from restapi.server import create_app
 from restapi.services.cache import Cache
-from restapi.utilities.meta import Meta
 from restapi.tests import API_URI, BaseTests, FlaskClient
 from restapi.utilities.logs import log
+from restapi.utilities.meta import Meta
+
 
 class TestApp(BaseTests):
     CACHE_FILE_NUMBER = 5
@@ -25,13 +25,10 @@ class TestApp(BaseTests):
         Cache.invalidate(Datasets.get)
         log.info("CACHE INVALIDATED")
 
-
-    def test_endpoint_access(self,client: FlaskClient, faker: Faker)-> None:
+    def test_endpoint_access(self, client: FlaskClient, faker: Faker) -> None:
         # create a fake user and login with it
 
-        uuid, data = self.create_user(
-            client, {"roles":["normal_user"]}
-        )
+        uuid, data = self.create_user(client, {"roles": ["normal_user"]})
         # Will be used to delete the user after the tests
         self.save("fake_uuid", uuid)
         user_header, _ = self.do_login(client, data.get("email"), data.get("password"))
@@ -45,7 +42,13 @@ class TestApp(BaseTests):
         # delete the newly created user
         uuid = self.get("fake_uuid")
         self.delete_user(client, uuid)
-    def test_not_existing_dataset(self,client: FlaskClient, faker: Faker,app: Flask,)-> None:
+
+    def test_not_existing_dataset(
+        self,
+        client: FlaskClient,
+        faker: Faker,
+        app: Flask,
+    ) -> None:
         # check if there is a cache dir and if not empty delete the content
         if not CACHE_DIR.is_dir():
             CACHE_DIR.mkdir(parents=True, exist_ok=True)
@@ -57,27 +60,24 @@ class TestApp(BaseTests):
         self.save("auth_header", admin_headers)
         headers = self.get("auth_header")
         endpoint = f"{API_URI}/admin/cache"
-        fake_dataset = {"clean":True,"datasets":[faker.pystr()]}
-        r = client.post(endpoint, json=fake_dataset,headers=headers)
+        fake_dataset = {"clean": True, "datasets": [faker.pystr()]}
+        r = client.post(endpoint, json=fake_dataset, headers=headers)
         assert r.status_code == 404
         # try to create the cache for a dataset that does not exists
-        self.send_task(
-            app,
-            "create_cache",
-            [faker.pystr()]
-        )
+        self.send_task(app, "create_cache", [faker.pystr()])
         # check that no cache has been created
         assert not any(CACHE_DIR.iterdir())
 
-    def test_cache_clean_with_empty_cache_dir(self,client: FlaskClient, faker: Faker,app: Flask)-> None:
+    def test_cache_clean_with_empty_cache_dir(
+        self, client: FlaskClient, faker: Faker, app: Flask
+    ) -> None:
         # delete cache dir
         CACHE_DIR.rmdir()
         # check system error if cache dir does not exists
-        with pytest.raises(Ignore, match=r"^(.*?(\bInvalid CACHE_PATH config\b)[^$]*)$"):
-            self.send_task(
-                app,
-                "clean_cache"
-            )
+        with pytest.raises(
+            Ignore, match=r"^(.*?(\bInvalid CACHE_PATH config\b)[^$]*)$"
+        ):
+            self.send_task(app, "clean_cache")
         # recreate the dir
         CACHE_DIR.mkdir(parents=True, exist_ok=True)
         # check clean with cache empty
@@ -109,7 +109,7 @@ class TestApp(BaseTests):
         # check that no cache has been created
         assert not any(CACHE_DIR.iterdir())
 
-    def test_cache_create(self,client: FlaskClient, faker: Faker,app: Flask)-> None:
+    def test_cache_create(self, client: FlaskClient, faker: Faker, app: Flask) -> None:
         # create a cache
         headers = self.get("auth_header")
         endpoint = f"{API_URI}/admin/cache"
@@ -145,13 +145,3 @@ class TestApp(BaseTests):
         # assert r.status_code == 200
         # response_body = self.get_content(r)
         # assert "Nothing to be done" in response_body
-
-
-
-
-
-
-
-
-
-
